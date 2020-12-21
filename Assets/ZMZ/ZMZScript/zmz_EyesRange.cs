acd9651b575bb6de3a66cff3d6ef1b9d341cae49
PC-20201002IMTW\Administrator <401941971@qq.com>
2020-12-21 15:01:13 +0800

    zmz测试代码

---
+++ Assets/ZMZ/ZMZScript/zmz_EyesRange.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zmz_EyesRange : MonoBehaviour
+{
+    public static bool IsTrriger;
+    private void Awake()
+    {
+        IsTrriger = false;
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("1");
+        if (collision.transform.tag == "enemy")
+            IsTrriger = true;
+        Debug.Log("2");
+
+    }
+    void OnTriggerExit(Collider collision)
+    {
+        if (collision.transform.tag == "enemy")
+            IsTrriger = false;
+    }
+}
